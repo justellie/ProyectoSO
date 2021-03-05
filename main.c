b29cdd685e88cbe7cca88da3009fe8d77976ef88
Elio <Elioruiz.en@gmail.com>
2021-03-05 12:43:21 -0400

    Fixed inicializacion

--- main.c
+++ main.c
@@ -155,33 +155,33 @@ int main(){
         pthread_create( act->Director + id,         // Thread-id reference.
                         NULL,                       // No special attributes.
                         &actor_director,            // routine.
-                        &Tabla_Hospitales + id );   // ref. attributes.
+                        Tabla_Hospitales + id );   // ref. attributes.
 
         pthread_create( act->Gestores + id,     // Thread-id reference.
                         NULL,                   // No special attributes.
                         &actor_gestor,          // routine.
-                        &Tabla_Gestores + id);  // ref. attributes.
+                        Tabla_Gestores + id);  // ref. attributes.
 
         pthread_create( act->JefeEpidemia + id, // Thread-id reference.
                         NULL,                   // No special attributes.
                         &actor_jefe_epidemia,   // routine.
-                        &Tabla_Hospitales + id);// ref. attributes.
+                        Tabla_Hospitales + id);// ref. attributes.
 
         pthread_create( act->JefeCuidados + id, // Thread-id reference.
                         NULL,                   // No special attributes.
                         &actor_jefe_cuidados_intensivos, // routine
-                        &Tabla_Hospitales + id);// ref. attributes.
+                        Tabla_Hospitales + id);// ref. attributes.
 
         pthread_create( act->JefeAdmin + id,    // Thread-id reference.
                         NULL,                   // No special attributes.
                         &actor_jefe_admin,      // routine
-                        &Tabla_Hospitales + id);// ref. attributes.
+                        Tabla_Hospitales + id);// ref. attributes.
 
         for( int iter = 0 ; iter < NSALA_MUESTRA ; iter += 1 ){
             pthread_create( act->Analistas + analista,      // Thread-id reference.
                             NULL,                           // No special attributes.
                             &actor_analista,                // routine.
-                            &Tabla_Hospitales + analista ); // ref. attributes.
+                            Tabla_Hospitales + analista ); // ref. attributes.
             analista += 1;
         }
     }
